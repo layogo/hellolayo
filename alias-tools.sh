49910cffa0b96a8f254934b0c31979150680a5d5
yulin <yulin@nibirutech.com>
2014-02-19 15:28:56 +0800

    add alias tools.sh

---
+++ alias-tools.sh
@@ -0,0 +1,10 @@
+############## dir alias #############
+alias wp='cd /Users/yulin/workplace'
+alias ios='cd /Users/yulin/workplace/ios'
+alias an='cd /Users/yulin/workplace/android'
+
+
+#gt
+alias gt='cd /Users/yulin/workplace/ios/gt'
+alias gts='cd /Users/yulin/workplace/ios/gt/global-theat-server && r87
+############## dir alias #############
