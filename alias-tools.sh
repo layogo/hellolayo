19d1c6eadfc5e6b1d8795b035e20a2e890a7ded1
yulin <yulin@nibirutech.com>
2014-02-20 10:18:40 +0800

    add alias commond

--- alias-tools.sh
+++ alias-tools.sh
@@ -6,5 +6,38 @@ alias an='cd /Users/yulin/workplace/android'
 
 #gt
 alias gt='cd /Users/yulin/workplace/ios/gt'
-alias gts='cd /Users/yulin/workplace/ios/gt/global-theat-server && r87
+alias gts='cd /Users/yulin/workplace/ios/gt/global-theat-server && r87'
+#ge
+alias ge='cd /Users/yulin/workplace/ios/ge'
+alias ges='cd /Users/yulin/workplace/ios/ge/galaxy-server && r20'
+#ange
+alias age='cd /Users/yulin/workplace/android/ge'
+alias ages='cd /Users/yulin/workplace/android/ge/galaxy-server && r87'
 ############## dir alias #############
+
+# zsh 
+alias cls='clear'
+alias ll='ls -l'
+alias la='ls -a'
+alias vd='vimdiff '
+#alias vi='vim'
+alias javac="javac -J-Dfile.encoding=utf8"
+alias grep="grep --color=auto"
+alias -s html=mate   # 在命令行直接输入后缀为 html 的文件名，会在 TextMate 中打开
+alias -s rb=mate     # 在命令行直接输入 ruby 文件，会在 TextMate 中打开
+alias -s py=vi       # 在命令行直接输入 python 文件，会用 vim 中打开，以下类似
+alias -s js=vi
+alias -s c=vi
+alias -s java=vi
+alias -s txt=vi
+alias -s gz='tar -xzvf'
+alias -s tgz='tar -xzvf'
+alias -s zip='unzip'
+alias -s bz2='tar -xjvf'
+# end zsh
+#rvm
+alias r87='rvm use 1.8.7@rails320'
+alias r20='rvm use 2.0.0@rails40'
+
+#git
+alias gs='git status'
